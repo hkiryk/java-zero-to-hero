29ed8c0cc1e472deb6658a4b9c1b412b04fadb4a
user <misskiriko97@gmail.com>
2019-03-10 14:19:55 +0200

    test

--- src/main/java/ee/itcollage/level12/CarPicker.java
+++ src/main/java/ee/itcollage/level12/CarPicker.java
@@ -1,5 +1,6 @@
 package ee.itcollage.level12;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -16,22 +17,46 @@ public class CarPicker {
                 buildCar("Lada", 5000, false, 2.0),
                 buildCar("Bicycle", 100, false, 0.5)
         );
-        System.out.println("Garage contains: \n" + garage);
+        System.out.println("Garage contains: \n");
+        for (Car car : garage) {
+            System.out.println(car.getModel());
+        }
+
+
         //todo 3.1 what does garage print? Can you make it print something prettier?
 
         //todo 3.2 Ask user how much money do they have and pick them a car or cars based on cars you have in garage
         // uncomment following lines for reading input example
         Scanner scanner = new Scanner(System.in);
-        //System.out.println("How much money do you have?");
-        //String input = scanner.nextLine();
-        //System.out.println("Your input: " + input);
+        System.out.println("How much money do you have?");
+        Integer input = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Minimal user rating(0-10)?");
+        Double input1 = scanner1.nextDouble();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Do you want electric car?");
+        String input2 = scanner2.nextLine();
 
+        boolean elc;
+        if (input2.equals("yes")) elc = true;
+        else elc = false;
+       // System.out.println(elc);
 
-        Car car = garage.get(0);
+        List <Car> recommendation = new ArrayList<>();
+        for (Car car : garage) {
+            if (car.getCost()<= input && car.getUserRating()>=input1 && elc == car.isElectric()){
+                recommendation.add(car);
+            }
+        }
 
-        System.out.println("\n");
-        System.out.println("Shop recommends you: " + car);
-
+        if (recommendation.isEmpty()) System.out.println("Shop can not recommend you anything");
+        else {
+            System.out.println("\n");
+            System.out.println("Shop recommends you: ");
+            for (Car car : recommendation) {
+                System.out.println(car.getModel());
+            }
+        }
         //todo ADVANCED
         // ask user 2nd question (+rating)
 
